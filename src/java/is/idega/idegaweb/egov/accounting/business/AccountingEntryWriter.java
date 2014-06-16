5a7c578ec7de4c7d8ee4d5fa7de652b4f20b4896
Valdas Žemaitis <valdas@idega.com>
2014-06-16 11:23:51 +0300

    Cleaning sheet's name

--- src/java/is/idega/idegaweb/egov/accounting/business/AccountingEntryWriter.java
+++ src/java/is/idega/idegaweb/egov/accounting/business/AccountingEntryWriter.java
@@ -40,6 +40,7 @@ import com.idega.presentation.IWContext;
 import com.idega.util.IWTimestamp;
 import com.idega.util.PersonalIDFormatter;
 import com.idega.util.StringHandler;
+import com.idega.util.text.TextSoap;
 
 
 public class AccountingEntryWriter extends DownloadWriter implements MediaWritable {
@@ -48,22 +49,22 @@ public class AccountingEntryWriter extends DownloadWriter implements MediaWritab
 	private Locale locale;
 	private IWResourceBundle iwrb;
 	private String caseCode = null;
-	
+
 	public static final String PARAMETER_CASE_CODE = "prm_case_code";
-	
+
 	public AccountingEntryWriter() {
 	}
-	
+
 	@Override
 	public void init(HttpServletRequest req, IWContext iwc) {
 		try {
 			this.locale = iwc.getApplicationSettings().getApplicationLocale();
 			this.iwrb = iwc.getIWMainApplication().getBundle(AccountingConstants.IW_BUNDLE_IDENTIFIER).getResourceBundle(this.locale);
 			this.caseCode = iwc.getParameter(PARAMETER_CASE_CODE);
-			
+
 			Map paymentMethod = (Map) iwc.getSessionAttribute(AccountingConstants.SESSION_PAYMENT_METHOD_MAP);
 			Map products = (Map) iwc.getSessionAttribute(AccountingConstants.SESSION_PRODUCT_MAP);
-				
+
 			this.buffer = writeXLS(iwc, paymentMethod, products);
 			setAsDownload(iwc,"students.xls",this.buffer.length());
 		}
@@ -71,7 +72,7 @@ public class AccountingEntryWriter extends DownloadWriter implements MediaWritab
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public String getMimeType() {
 		if (this.buffer != null) {
@@ -79,7 +80,7 @@ public class AccountingEntryWriter extends DownloadWriter implements MediaWritab
 		}
 		return super.getMimeType();
 	}
-	
+
 	@Override
 	public void writeTo(OutputStream out) throws IOException {
 		if (this.buffer != null) {
@@ -94,7 +95,7 @@ public class AccountingEntryWriter extends DownloadWriter implements MediaWritab
 			System.err.println("buffer is null");
 		}
 	}
-	
+
 	public MemoryFileBuffer writeXLS(IWContext iwc, Map paymentMethod, Map products) throws Exception {
 		MemoryFileBuffer buffer = new MemoryFileBuffer();
 		MemoryOutputStream mos = new MemoryOutputStream(buffer);
@@ -102,10 +103,10 @@ public class AccountingEntryWriter extends DownloadWriter implements MediaWritab
 		if (paymentMethod != null && products != null) {
 	    HSSFWorkbook wb = new HSSFWorkbook();
 
-	    HSSFSheet sheet = wb.createSheet(StringHandler.shortenToLength(this.iwrb.getLocalizedString("accounting_statistics.statistics", "Statistics"), 30));
+	    HSSFSheet sheet = wb.createSheet(TextSoap.encodeToValidExcelSheetName(StringHandler.shortenToLength(this.iwrb.getLocalizedString("accounting_statistics.statistics", "Statistics"), 30)));
 	    sheet.setColumnWidth((short)0, (short) (30 * 256));
 	    sheet.setColumnWidth((short)1, (short) (14 * 256));
-	    
+
 			HSSFFont font = wb.createFont();
 	    font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
 	    font.setFontHeightInPoints((short)12);
@@ -113,23 +114,23 @@ public class AccountingEntryWriter extends DownloadWriter implements MediaWritab
 	    style.setFont(font);
 
 	    int cellRow = 0;
-	    
+
 			HSSFRow row = sheet.createRow(cellRow++);
 	    HSSFCell cell = row.createCell((short)0);
 	    cell.setCellValue(this.iwrb.getLocalizedString("accounting_statistics.item", "Item"));
 	    cell.setCellStyle(style);
-	    
+
 	    cell = row.createCell((short)1);
 	    cell.setCellValue(this.iwrb.getLocalizedString("accounting_statistics.total", "Total"));
 	    cell.setCellStyle(style);
-			
+
 			Collection keys = paymentMethod.keySet();
 			Iterator iter = keys.iterator();
 			while (iter.hasNext()) {
 				row = sheet.createRow(cellRow++);
 				String key = (String) iter.next();
 				int count = ((Collection) paymentMethod.get(key)).size();
-				
+
 				cell = row.createCell((short)0);
 		    cell.setCellValue(this.iwrb.getLocalizedString("accounting_statistics.payment_method." + key, key));
 
@@ -143,7 +144,7 @@ public class AccountingEntryWriter extends DownloadWriter implements MediaWritab
 				row = sheet.createRow(cellRow++);
 				String key = (String) iter.next();
 				int count = ((Collection) products.get(key)).size();
-				
+
 				cell = row.createCell((short)0);
 		    cell.setCellValue(this.iwrb.getLocalizedString("accounting_statistics." + this.caseCode + "." + key, key));
 
@@ -157,13 +158,13 @@ public class AccountingEntryWriter extends DownloadWriter implements MediaWritab
 				String key = (String) iter.next();
 				createNewSheet(iwc, wb, iwrb.getLocalizedString("accounting_statistics." + this.caseCode + "." + key, key), (List) products.get(key));
 			}
-	    
+
 			wb.write(mos);
 		}
 		buffer.setMimeType(MimeTypeUtil.MIME_TYPE_EXCEL_2);
 		return buffer;
 	}
-	
+
 	private void createNewSheet(IWContext iwc, HSSFWorkbook workbook, String sheetName, List entries) {
 		Collections.sort(entries, new AccountingEntryComparator(iwc.getCurrentLocale()));
 
@@ -172,13 +173,13 @@ public class AccountingEntryWriter extends DownloadWriter implements MediaWritab
 		format.setMinimumFractionDigits(0);
 		format.setGroupingUsed(false);
 
-    HSSFSheet sheet = workbook.createSheet(StringHandler.shortenToLength(sheetName, 30));
+    HSSFSheet sheet = workbook.createSheet(TextSoap.encodeToValidExcelSheetName(StringHandler.shortenToLength(sheetName, 30)));
     sheet.setColumnWidth((short)0, (short) (30 * 256));
     sheet.setColumnWidth((short)1, (short) (14 * 256));
     sheet.setColumnWidth((short)2, (short) (14 * 256));
     sheet.setColumnWidth((short)3, (short) (14 * 256));
 		sheet.setColumnWidth((short)4, (short) (14 * 256));
-    
+
 		HSSFFont font = workbook.createFont();
     font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
     font.setFontHeightInPoints((short)12);
@@ -196,27 +197,27 @@ public class AccountingEntryWriter extends DownloadWriter implements MediaWritab
 		cell = row.createCell(cellColumn++);
     cell.setCellValue(this.iwrb.getLocalizedString("accounting_statistics.personal_id","Personal ID"));
     cell.setCellStyle(style);
-    
+
     cell = row.createCell(cellColumn++);
     cell.setCellValue(this.iwrb.getLocalizedString("accounting_statistics.valid_from", "Valid from"));
     cell.setCellStyle(style);
-    
+
     cell = row.createCell(cellColumn++);
     cell.setCellValue(this.iwrb.getLocalizedString("accounting_statistics.valid_to", "Valid to"));
     cell.setCellStyle(style);
-		
+
     cell = row.createCell(cellColumn++);
     cell.setCellValue(this.iwrb.getLocalizedString("accounting_statistics.units", "Units"));
     cell.setCellStyle(style);
-    
+
 		Iterator iter = entries.iterator();
 		while (iter.hasNext()) {
 			AccountingEntry entry = (AccountingEntry) iter.next();
 			row = sheet.createRow(cellRow++);
 			cellColumn = 0;
-			
+
 			IWTimestamp startDate = new IWTimestamp(entry.getStartDate());
-			
+
 			IWTimestamp endDate = null;
 			if (entry.getEndDate() != null) {
 				endDate = new IWTimestamp(entry.getEndDate());
