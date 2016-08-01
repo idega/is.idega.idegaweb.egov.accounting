982b8ec9872217e09ebf8a47774e62616b47b505
Valdas Žemaitis <valdas@idega.com>
2016-08-01 09:27:32 +0300

    Improvements

--- src/java/is/idega/idegaweb/egov/accounting/business/NavisionBusinessBean.java
+++ src/java/is/idega/idegaweb/egov/accounting/business/NavisionBusinessBean.java
@@ -1,29 +1,32 @@
 /*
  * $Id: NavisionBusinessBean.java,v 1.8 2008/05/10 11:52:04 valdas Exp $ Created
  * on Jul 12, 2006
- * 
+ *
  * Copyright (C) 2006 Idega Software hf. All Rights Reserved.
- * 
+ *
  * This software is the proprietary information of Idega hf. Use is subject to
  * license terms.
  */
 package is.idega.idegaweb.egov.accounting.business;
-
-import is.idega.idegaweb.egov.accounting.data.CaseCodeAccountingKey;
-import is.idega.idegaweb.egov.accounting.wsimpl.WiseWebServiceLocator;
-import is.idega.idegaweb.egov.accounting.wsimpl.WiseWebServiceSoap_PortType;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.logging.Level;
 
 import javax.xml.rpc.ServiceException;
 
+import com.idega.idegaweb.IWApplicationContext;
 import com.idega.idegaweb.IWMainApplication;
+import com.idega.idegaweb.IWMainApplicationSettings;
 import com.idega.util.IWCalendar;
 import com.idega.util.IWTimestamp;
+
+import is.idega.idegaweb.egov.accounting.data.CaseCodeAccountingKey;
+import is.idega.idegaweb.egov.accounting.wsimpl.WiseWebServiceLocator;
+import is.idega.idegaweb.egov.accounting.wsimpl.WiseWebServiceSoap_PortType;
 
 /**
  * This class extends AccountingKeyBusinessBean so we can call a webservice when
@@ -35,7 +38,7 @@ import com.idega.util.IWTimestamp;
  * The last one can be used to force a month to process by setting it to a date
  * of the last day of that month (2006-11-30) Last modified: $Date: 2006/11/23
  * 12:07:48 $ by $Author: valdas $
- * 
+ *
  * @author <a href="mailto:eiki@idega.com">eiki</a>
  * @version $Revision: 1.8 $
  */
@@ -45,15 +48,15 @@ public class NavisionBusinessBean extends AccountingKeyBusinessBean implements N
 	public static final String MARITECH_NAV_LASTMONTHSENT = "maritech.nav.lastmonthsent";
 	public static final String MARITECH_NAV_LASTMONTHFAILED = "maritech.nav.lastmonthfailed";
 	public static final String MARITECH_NAVISION_ALLOWED_CASE_CODES = "maritech.nav.month.casecodes";
-	
+
 	/**
 	 * The date of the last month sent "ahead" in time e.g. december records will be sent on the 1.december for the whole month of December
 	 */
 	public static final String MARITECH_NAV_LAST_FUTURE_MONTHSENT = "maritech.nav.lastFutureMSent";
 	public static final String MARITECH_NAV_LAST_FUTURE_MONTHFAILED = "maritech.nav.lastFutureMFail";
 	public static final String MARITECH_NAVISION_ALLOWED_FUTURE_MONTH_CASE_CODES = "maritech.nav.futuremonthccodes";
-	
-	
+
+
 	public static final String MARITECH_NAV_FAKE_CURRENT_DATE = "maritech.nav.fakecurrentdate";
 
 	/**
@@ -67,6 +70,7 @@ public class NavisionBusinessBean extends AccountingKeyBusinessBean implements N
 	/**
 	 * Overrides the method and calls a webservice for each accountingstring
 	 */
+	@Override
 	protected void onGeneratedAccountingString(String generatedAccountingString, AccountingEntry entry, CaseCodeAccountingKey key, String accountingSystem, IWTimestamp fromStamp, IWTimestamp toStamp) {
 
 		if (isActive()) {
@@ -136,10 +140,11 @@ public class NavisionBusinessBean extends AccountingKeyBusinessBean implements N
 	 * with the webservice to maritech. Filters the allowed casecodes from an application property
 	 * "maritech.nav.month.casecodes" which is a comma seperated case code string of allowed case codes.
 	 * If the string is empty it sends ALL case codes.
-	 * 
+	 *
 	 * @param now
 	 * @return
 	 */
+	@Override
 	public void sendAllAccountingEntriesForMonth(IWTimestamp stamp) {
 
 		log("STARTING to send account entries to Navision (" + stamp.toString() + ")");
@@ -180,13 +185,13 @@ public class NavisionBusinessBean extends AccountingKeyBusinessBean implements N
 		}
 
 	}
-	
+
 	/**
 	 * Gets all account entries for all casecodes for a future month (on the 1st of every month) and sends them
 	 * with the webservice to maritech. Filters the allowed casecodes from an application property
 	 * "maritech.nav.futuremonthccodes" which is a comma seperated case code string of allowed case codes.
 	 * If the string is empty it sends NO case codes this is the biggest difference from the current month method
-	 * 
+	 *
 	 * @param now
 	 * @return
 	 */
@@ -195,7 +200,7 @@ public class NavisionBusinessBean extends AccountingKeyBusinessBean implements N
 		log("STARTING to send future account entries to Navision (" + stamp.toString() + ")");
 		String futureCaseCodes = this.getIWApplicationContext().getApplicationSettings().getProperty(MARITECH_NAVISION_ALLOWED_FUTURE_MONTH_CASE_CODES, "");
 		boolean anythingSent = false;
-		
+
 		// get all the case codes and for each do:
 		Collection caseCodes;
 		try {
@@ -230,14 +235,14 @@ public class NavisionBusinessBean extends AccountingKeyBusinessBean implements N
 		}
 
 	}
-	
+
 	/**
 	 * @param stamp
 	 */
 	protected void setMonthAsLastSent(IWTimestamp stamp) {
 		this.getIWApplicationContext().getApplicationSettings().setProperty(MARITECH_NAV_LASTMONTHSENT, Integer.toString(stamp.getMonth()));
 	}
-	
+
 	/**
 	 * @param stamp
 	 */
@@ -251,7 +256,7 @@ public class NavisionBusinessBean extends AccountingKeyBusinessBean implements N
 	protected void setMonthAsLastFailed(IWTimestamp stamp) {
 		this.getIWApplicationContext().getApplicationSettings().setProperty(MARITECH_NAV_LASTMONTHFAILED, Integer.toString(stamp.getMonth()));
 	}
-	
+
 	/**
 	 * @param stamp
 	 */
@@ -259,25 +264,42 @@ public class NavisionBusinessBean extends AccountingKeyBusinessBean implements N
 		this.getIWApplicationContext().getApplicationSettings().setProperty(MARITECH_NAV_LAST_FUTURE_MONTHFAILED, Integer.toString(stamp.getMonth()));
 	}
 
+	@Override
 	public void actionPerformed(ActionEvent e) {
-		IWTimestamp now = null;
-		// fake current date option so we can force a month to process
-		String fakeCurrentDate = IWMainApplication.getDefaultIWApplicationContext().getApplicationSettings().getProperty(MARITECH_NAV_FAKE_CURRENT_DATE);
-		if (fakeCurrentDate != null) {
-			now = new IWTimestamp(fakeCurrentDate);
-		}
-		else {
-			now = new IWTimestamp();
-		}
+		try {
+			IWApplicationContext iwac = IWMainApplication.getDefaultIWApplicationContext();
+			if (iwac == null) {
+				getLogger().warning(IWApplicationContext.class.getName() + " can not be resolved");
+				return;
+			}
 
-		if(e.getActionCommand().equals(AccountingConstants.ACCOUNTING_SYSTEM_NAVISION_XML) && isActive() ){
-			if (canSendForMonth(now)) {
-				//todo possible create a new thread that waits till the last minute of the day to send?
-				sendAllAccountingEntriesForMonth(now);
+			IWMainApplicationSettings settings = iwac.getApplicationSettings();
+			if (settings == null) {
+				getLogger().warning(IWMainApplicationSettings.class.getName() + " can not be resolved");
+				return;
 			}
-			else if (canSendForFutureMonth(now)) {
-				sendAllAccountingEntriesForFutureMonth(now);
+
+			IWTimestamp now = null;
+			// fake current date option so we can force a month to process
+			String fakeCurrentDate = settings.getProperty(MARITECH_NAV_FAKE_CURRENT_DATE);
+			if (fakeCurrentDate != null) {
+				now = new IWTimestamp(fakeCurrentDate);
 			}
+			else {
+				now = new IWTimestamp();
+			}
+
+			if(e.getActionCommand().equals(AccountingConstants.ACCOUNTING_SYSTEM_NAVISION_XML) && isActive() ){
+				if (canSendForMonth(now)) {
+					//todo possible create a new thread that waits till the last minute of the day to send?
+					sendAllAccountingEntriesForMonth(now);
+				}
+				else if (canSendForFutureMonth(now)) {
+					sendAllAccountingEntriesForFutureMonth(now);
+				}
+			}
+		} catch (Exception ex) {
+			getLogger().log(Level.WARNING, "Error performing event: " + e, e);
 		}
 	}
 
@@ -297,6 +319,7 @@ public class NavisionBusinessBean extends AccountingKeyBusinessBean implements N
 		return isLastDayOfMonth(now) && !hasBeenSentForMonth(now);
 	}
 
+	@Override
 	public boolean isLastDayOfMonth(IWTimestamp stamp) {
 		IWCalendar calendar = new IWCalendar(stamp);
 		int daysInMonth = calendar.getLengthOfMonth(stamp.getMonth(), stamp.getYear());
@@ -304,12 +327,13 @@ public class NavisionBusinessBean extends AccountingKeyBusinessBean implements N
 
 		return (daysInMonth == thisDayOfMonth);
 	}
-	
+
 	public boolean isFirstDayOfMonth(IWTimestamp stamp) {
 		int thisDayOfMonth = stamp.getDay();
 		return (1==thisDayOfMonth);
 	}
 
+	@Override
 	public boolean hasBeenSentForMonth(IWTimestamp stamp) {
 		String lastMonthSent = this.getIWApplicationContext().getApplicationSettings().getProperty(MARITECH_NAV_LASTMONTHSENT);
 		// its a java.sql.date!
@@ -337,7 +361,7 @@ public class NavisionBusinessBean extends AccountingKeyBusinessBean implements N
 			return false;
 		}
 	}
-	
+
 	public boolean hasBeenSentForFutureMonth(IWTimestamp stamp) {
 		String lastMonthSent = this.getIWApplicationContext().getApplicationSettings().getProperty(MARITECH_NAV_LAST_FUTURE_MONTHSENT);
 		// its a java.sql.date!
@@ -347,6 +371,7 @@ public class NavisionBusinessBean extends AccountingKeyBusinessBean implements N
 	/**
 	 * @return
 	 */
+	@Override
 	public boolean isActive() {
 		String accountingSystem = this.getIWApplicationContext().getApplicationSettings().getProperty(AccountingConstants.PROPERTY_ACCOUNTING_SYSTEM);
 		String enabled = this.getIWApplicationContext().getApplicationSettings().getProperty(MARITECH_NAVISION_ENABLE);
